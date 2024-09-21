4b1811c5b155db79edb65311a07ff67a06c55af7
fon512 <denistolocin@gmail.com>
2024-09-21 18:27:14 +1000

    pycharm

---
+++ main.py
@@ -0,0 +1,16 @@
+#a = int(input("Введите кол-во секунд: "))
+#hours = 0
+#minutes = 0
+#seconds = 0
+#hours = a // 3600
+#minutes = (a % 3600) // 60
+#seconds = (a % 3600) % 60
+#print("Часы",hours , "минуты", minutes , "секунды", seconds )
+
+
+
+
+
+# print("Вася")
+# print("   пошёл")
+# print("       гулять")
