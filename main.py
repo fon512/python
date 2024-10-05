11636a89f14c96313b6ffafd811e046ffa4e953a
fon512 <denistolocin@gmail.com>
2024-10-05 18:28:48 +1000

    было зделено выбор действий

--- main.py
+++ main.py
@@ -4,9 +4,14 @@ import random
 
 hp = 100
 mana = 100
+
 strength = 5
 agility = 5
 intellect = 5
+
+damage = 5
+protection = 5
+
 lvl = 1
 
 print("Здоровье:", hp, "Мана:", mana)
@@ -21,17 +26,24 @@ _class = input("Выберите класс: 1. воин, 2. маг, 3. вор\n
 # > < == >= <= !=
 if _class == "воин" or _class == "1":
     strength = strength * 2
-    agility *= 1.6
-    _class = "Воин" 
-
-elif _class == "маг" or _class=="2":
+    agility *= 1.6 
+    protection += 5
+    damage += 2
+    hp += 50
+    _class="Воин"
+elif _class == "маг" or _class == "2":
     intellect *= 2
     agility *= 1.3
+    protection -=3
+    damage +=3
+    mana += 50
     _class="Маг"
-elif _class == "вор" or _class=="3":
+elif _class == "вор" or _class == "3":
     agility *= 2
     strength *= 1.5
     intellect *= 1.3
+    damage -= 1
+    protection -= 2
     _class="Вор"
 else: 
     print("ФАТАЛЬНАЯ ОШИБКА, ты НИЩИЙ.\nВсе характеристики понижены на 3")
@@ -42,38 +54,74 @@ else:
 
 print(f"Ваш новый класс: {_class}, новые характеристики:\nСила: {strength}\nЛовкость: {agility}\nИнтеллект: {intellect}")
 
-# ctrl + /
-
 # for i in range(100):
-#     print("Проход по циклу (итерация) №", i)
+#     print("проход по циклу (итерация) №", i)
 
-# n = 11
+# n = 0
 
 # while n<10:
-#     print("Цикл работает", n)
+#     print("цикл работает")
 #     n = n + 1
-# else: 
+# else:
 #     print("выполнился else")
 
-
-
 isGame = True
-# while isGame == True:
 while isGame:
+    # print("Игра идёт")
     # hp -= 1
     rand = random.randint(1,6)
 
-    print("выпало", rand)
+    print("Выпало", rand)
 
     if rand == 2:
-        print("На пути Тролль, -15хп")
-        hp -= 15
-        time.sleep(1.5)
+        hpEnemy = 20
+        damageEnemy = 7
+        print('перед вами Гоблин что делать?')
+        while True:
+            
+            choice = input('\n1. Атоковать 2. Защититься 3. увернутся\n')
+            
+            if choice == '1':
+                hpEnemy -= damage
+                hp -= damageEnemy // 1.5
+                print(f'Гоблин получил урон у него осталось {hpEnemy} hp')
+                print(f'хоть ты и ударил но и тебе тоже прилетело{damageEnemy // 1.5} урона\n у вас осталось {hp} hp')
+                print(f'Гоблин получил урон у него осталось {hpEnemy}' )
+            elif choice == "2":
+                if damageEnemy > damage:
+                    hp = hp - (damageEnemy-damage)
+                    print(f"Гоблин наносит {damageEnemy-damage} урон у вас осталось hp-{hp}")
+                else:
+                    print('Вы успешно заблокировали урон')    
+            elif choice == '3':
+
+                rand = random.randint(1,2)
+                if rand == 1:
+                    print('Успешный уворот')
+                else:
+                    hp -= damageEnemy
+                    print(f'Увернутся не получилось, вы полочили урон {damageEnemy}')
+            else:
+                hp -= damageEnemy
+                print(f'вы так и не выбрали правельного решения и вас ударили вы потеряли {damageEnemy}hp')
+            if hp <= 0:
+                break
+            elif hpEnemy <=0:
+                print('вы победили')
+                break
+    time.sleep(1.5)            
+            
+
     print("Остаток здоровья:", hp)
     if hp <= 0:
-        print("ПОТРАЧЕНО")
+        print("GAME OVER")
         isGame = False
         break
     time.sleep(0.5)
-    
-print("Игра завершена")
+
+
+
+
+
+
+
